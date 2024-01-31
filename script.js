39436b1c7ca84672be2d2d9110b3ffa7ccc1849b
Joy Onugha <joyonugha@gmail.com>
2024-01-31 22:50:18 +0000

    made adjustments to all the bugs

--- script.js
+++ script.js
@@ -19,36 +19,39 @@ $('.saveBtn').on('click', function () {
 });
 //Function for the current day (Ref: https://stackoverflow.com/questions/1531093/how-do-i-get-the-current-date-in-javascript)
 function currentDay () {
-    var currentDay = dayjs().format('MMMM D, YYYY')
+    var currentDay = dayjs().format('MMMM DD, YYYY')
     $("#currentDay").text("Current Date: " + currentDay)
     console.log(currentDay);
 }
 currentDay()
 //Function for the colour renditions (Ref: as directed by TA)
-function renderColors () {
-    var currentHour = day.js().hour();
-    $("#hour-9").addClass("present");
+function renderColors() {
+    var currentHour = dayjs().hour();
+    // $("#hour-9").addClass("present");
     
-// function colorConsoleLog(msg, color) {
-//     console.log("%c" + msg, "color:" + color + ";font-size:large;");
-// }
+function colorConsoleLog(msg, color) {
+    console.log("%c" + msg, "color:" + color + ";font-size:large;");
+}
 
-// // Log the color for the past, present, and future classes
-// colorConsoleLog("Past color: #d3d3d3", "#d3d3d3");
-// colorConsoleLog("Present color: #ff6961", "#ff6961");
-// // colorConsoleLog("Future color: #77dd77", "#77dd77");
-// });
-};
-renderColors ()
+// Log the color for the past, present, and future classes
+colorConsoleLog("Past: #d3d3d3", "#d3d3d3");
+colorConsoleLog("Present: #ff6961", "#ff6961");
+colorConsoleLog("Future: #77dd77", "#77dd77");
+}
+
+renderColors()
 
 function hourUpdater() {
     //Gets current number of hours
-    var currentHour = day.js().hour();
+    var currentHour = 9
+//dayjs().hour(); (I commented this out because I want it to display the colours)
+    
 
     //loops through time blocks
     $('.time-block').each(function () {
         var blockHour = parseInt($(this).attr('id').split('-')[1]);
-
+        console.log(blockHour);
+        console.log(currentHour);
         //This function would checks if we have gone past this time
         if (blockHour < currentHour) {
             $(this).addClass('past');
@@ -62,21 +65,21 @@ function hourUpdater() {
         }
     });
 }
-// hourUpdater();
+hourUpdater();
 
 //sets up interval to check if current time needs to be updated
 var interval = setInterval(hourUpdater, 15000);
 
-//loads any saved data from localStorage (Ref: Based on the earlier class work)
-// $('#hour-9 .description').val(localStorage.getItem('hour-9'));
-// $('#hour-10 .description').val(localStorage.getItem('hour-10'));
-// $('#hour-11 .description').val(localStorage.getItem('hour-11'));
-// $('#hour-12 .description').val(localStorage.getItem('hour-12'));
-// $('#hour-13 .description').val(localStorage.getItem('hour-13'));
-// $('#hour-14 .description').val(localStorage.getItem('hour-14'));
-// $('#hour-15 .description').val(localStorage.getItem('hour-15'));
-// $('#hour-16 .description').val(localStorage.getItem('hour-16'));
-// $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+// loads any saved data from localStorage (Ref: Based on the earlier class work)
+$('#hour-9 .description').val(localStorage.getItem('hour-9'));
+$('#hour-10 .description').val(localStorage.getItem('hour-10'));
+$('#hour-11 .description').val(localStorage.getItem('hour-11'));
+$('#hour-12 .description').val(localStorage.getItem('hour-12'));
+$('#hour-13 .description').val(localStorage.getItem('hour-13'));
+$('#hour-14 .description').val(localStorage.getItem('hour-14'));
+$('#hour-15 .description').val(localStorage.getItem('hour-15'));
+$('#hour-16 .description').val(localStorage.getItem('hour-16'));
+$('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
 //Displays current day on page
 $('#currentDay').text(dayjs().format('dddd, MMMM DD'));
